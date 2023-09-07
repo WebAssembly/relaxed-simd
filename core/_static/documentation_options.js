6821dd9f5de5b223452f4f3e1531c4784b97dfbd
ngzhian <ngzhian@users.noreply.github.com>
2023-09-07 01:12:51 +0000

    deploy: cd9c447623cc80f708023426b3fda85194602e11

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2023-06-30)',
+    VERSION: '2.0 (Draft 2023-09-07)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
